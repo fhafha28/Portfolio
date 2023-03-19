55ab94361c7da70ccfdb10a81afff98419d08672
fhafha28 <96311086+fhafha28@users.noreply.github.com>
2023-03-19 16:30:59 +0100

    Create AppServiceProvider.php

---
+++ Laravel/contactApp/app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrapFour();
+//        if(env('APP_DEBUG')){
+//            DB::listen(function ($query){
+//                Log::info($query->sql, $query->bindings, $query->time);
+//            });
+//        }
+    }
+}
