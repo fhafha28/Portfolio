ee35454e98a450ef6328dfbe41e1199881cb1f5e
fhafha28 <96311086+fhafha28@users.noreply.github.com>
2023-03-19 16:28:48 +0100

    Create Company.php

---
+++ Laravel/contactApp/app/Models/Company.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\AllowedFilterSearch;
+use App\Models\Scopes\AllowedSort;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, SoftDeletes, AllowedFilterSearch, AllowedSort;
+    protected $guarded=[];
+//    protected $fillable = ['name', 'website', 'email', 'address'];
+
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
